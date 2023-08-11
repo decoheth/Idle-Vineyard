4cee1db0d5e7b225ecf37fd6eb536156f6014649
decoheth <hetheringtond.dh@gmail.com>
2023-08-12 00:06:10 +0100

    Minor tweaks

--- Assets/Scripts/Ads/AdsManager.cs
+++ Assets/Scripts/Ads/AdsManager.cs
@@ -69,44 +69,43 @@ public class AdsManager : MonoBehaviour
 
 
 
-    private void RegisterEventHandlers(RewardedAd ad)
+private void RegisterEventHandlers(RewardedAd ad)
+{
+    // Raised when the ad is estimated to have earned money.
+    ad.OnAdPaid += (AdValue adValue) =>
     {
-        // Raised when the ad is estimated to have earned money.
-        ad.OnAdPaid += (AdValue adValue) =>
-        {
-            Debug.Log(String.Format("Rewarded ad paid {0} {1}.",
-                adValue.Value,
-                adValue.CurrencyCode));
-        };
-        // Raised when an impression is recorded for an ad.
-        ad.OnAdImpressionRecorded += () =>
-        {
-            Debug.Log("Rewarded ad recorded an impression.");
-        };
-        // Raised when a click is recorded for an ad.
-        ad.OnAdClicked += () =>
-        {
-            Debug.Log("Rewarded ad was clicked.");
-        };
-        // Raised when an ad opened full screen content.
-        ad.OnAdFullScreenContentOpened += () =>
-        {
-            Debug.Log("Rewarded ad full screen content opened.");
-        };
-        // Raised when the ad closed full screen content.
-        ad.OnAdFullScreenContentClosed += () =>
-        {
-            Debug.Log("Rewarded ad full screen content closed.");
-            LoadRewardedAd();
-        };
-        // Raised when the ad failed to open full screen content.
-        ad.OnAdFullScreenContentFailed += (AdError error) =>
-        {
-            Debug.LogError("Rewarded ad failed to open full screen content " +
-                           "with error : " + error);
-            LoadRewardedAd();
-        };
-    }
+        Debug.Log(String.Format("Rewarded ad paid {0} {1}.",
+            adValue.Value,
+            adValue.CurrencyCode));
+    };
+    // Raised when an impression is recorded for an ad.
+    ad.OnAdImpressionRecorded += () =>
+    {
+        Debug.Log("Rewarded ad recorded an impression.");
+    };
+    // Raised when a click is recorded for an ad.
+    ad.OnAdClicked += () =>
+    {
+        Debug.Log("Rewarded ad was clicked.");
+    };
+    // Raised when an ad opened full screen content.
+    ad.OnAdFullScreenContentOpened += () =>
+    {
+        Debug.Log("Rewarded ad full screen content opened.");
+    };
+    // Raised when the ad closed full screen content.
+    ad.OnAdFullScreenContentClosed += () =>
+    {
+        Debug.Log("Rewarded ad full screen content closed.");
+    };
+    // Raised when the ad failed to open full screen content.
+    ad.OnAdFullScreenContentFailed += (AdError error) =>
+    {
+        Debug.LogError("Rewarded ad failed to open full screen content " +
+                       "with error : " + error);
+    };
+}
+
 
     // Update is called once per frame
     void Update()
